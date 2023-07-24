31d21c93f8614692d9fb690f7fcad60e5cd7156b
Ryan 王 <b20031020@gmail.com>
2023-07-25 00:21:54 +0800

    add print two numbers

---
+++ 3.Print-Two-Numbers/Print-Two-Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(int argc, char *argv[]) {
+ 
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",b);
+    printf("%d\n",a);
+ 
+ 
+    return 0;
+}
