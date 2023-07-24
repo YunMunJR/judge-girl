dd73fe5b8a7b0e2c2b5ee022ea0ad3d8e9cae6e5
Ryan 王 <b20031020@gmail.com>
2023-07-25 01:27:36 +0800

    combine them into a subset

---
+++ Basic IO and Arithmetic/27. Surface Area and Volume of a Box/Surface Area and Volume of a Box.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char** argv) {
+    int a,b,c,d,e;
+      scanf("%d",&a);
+      scanf("%d",&b);
+      scanf("%d",&c);
+      scanf("%d",&d);
+      scanf("%d",&e);
+    int a_ = a - 2*e, b_ = b - 2*e, c_ = c - 2*e;
+    int out_area = 2 * (a * b + b * c + c * a);
+    int in_area = 8 * d * (a_ + b_ + c_);
+    int vol = a * b * c - 2 * d * (a_ * b_ + b_ * c_ + c_ * a_);
+     printf("%d\n", out_area+in_area);
+    printf("%d\n",vol);
+ 
+ 
+    return 0;
+}
---
+++ Basic IO and Arithmetic/6. Print Three Digits/Print Three Digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(int argc, char *argv[]) {
+ 
+    int a;
+    scanf("%d",&a);
+    if(a<0)
+    a=-a;
+    int d=a/100;
+    a=a-d*100;
+    printf("%d\n",d);
+    int n=a/10;
+    printf("%d\n",n);
+    a=a-n*10;
+    printf("%d\n",a);
+ 
+ 
+ 
+    return 0;
+}
