58271659e58a6a6cde843d243e68fb631640baf7
Ryan 王 <b20031020@gmail.com>
2023-09-01 19:25:17 +0800

    modify some contents

--- Advanced/10069. Build a Binary Search Tree (Simple Object System Ver.)/my solution.c
+++ Advanced/10069. Build a Binary Search Tree (Simple Object System Ver.)/my solution.c
@@ -25,11 +25,12 @@ int bstNode_init(bstNode* obj) {
     obj->setLson = bstNode_setLson;
     obj->setRson = bstNode_setRson;
     obj->print = bstNode_print;
+    obj->lson = NULL, obj->rson = NULL;
     obj->extra = (void*)malloc(sizeof(int));
     return 1;
 }
 void bstNode_destroy(bstNode* obj) {
-    free(obj->extra);
+    //free(obj->extra);
 }
 
 Object bstNodeProto = {
@@ -37,6 +38,27 @@ Object bstNodeProto = {
     .destroy = bstNode_destroy
 };
 
+bstNode* genNode(int* A, int idx) {
+    bstNode* node;
+    node = NEW(bstNode);
+    node->extra = A + idx;
+    return node;
+}
+
+bstNode* insertNode(int idx, int A[], bstNode* root) {   
+    if (!root) { return genNode(A, idx); }
+    else {
+        if (A[idx] > *(int*)(root->extra)) root->rson = insertNode(idx, A, root->rson);
+        else root->lson = insertNode(idx, A, root->lson);
+    }
+    return root;
+}
+
 bstNode* buildBST(int A[], int n) {
     /* add your code */
+    bstNode* root = NULL;
+    for (int i = 0; i < n; i++) {       
+        root = insertNode(i, A, root);
+    }
+    return root;
 }
