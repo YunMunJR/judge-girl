5e0686bc12c59bef99fa7ad2d2c08f7f26975560
RyanW <109492396+WjrHyl@users.noreply.github.com>
2024-01-28 00:51:56 +0800

    Add files via upload

---
+++ C2022/50237/main.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#define MAXN 100
+typedef struct{
+    int x;
+    int y;
+    int z;
+    int empty;
+}data;
+typedef struct{
+    int x1;
+    int y1;
+    int z1;
+    int x2;
+    int y2;
+    int z2;
+}repeat;
+int max(int a,int b){
+    return (a>b)?a:b;
+}
+int f(char *s,int K){
+    int re=(int)(s[0]-'a')+1;
+    for(int i=1;i<strlen(s);i++){
+       re*=29;
+       re+=(int)(s[i]-'a'); 
+       re++;   
+    }
+    return re%K;
+}
+int main(){
+    int N,K,C;  
+    repeat re;
+    char cubic[MAXN][MAXN][MAXN][6];
+    scanf("%d",&N);
+    K=N*N;C=10;
+    data hash[K][C];
+    int repeat=0,hashcount[K];
+    for(int i=0;i<K;i++){
+        hashcount[i]=0;
+        for(int j=0;j<C;j++){
+            hash[i][j].empty=1; 
+            hash[i][j].x=-1;
+            hash[i][j].y=-1;
+            hash[i][j].z=-1;    
+        }
+    } 
+    for(int i=0;i<N;i++){
+        for(int j=0;j<=i;j++){
+            for(int k=0;k<=i;k++){
+                scanf("%s",cubic[i][j][k]);
+                if(j==i||k==i){
+                    repeat=0;                       
+                    int idx=f(cubic[i][j][k],K);
+                    for(int ii=0;ii<hashcount[idx];ii++){
+                        if(!strcmp(cubic[hash[idx][ii].x][hash[idx][ii].y][hash[idx][ii].z],cubic[i][j][k])&&!hash[idx][ii].empty){                
+                            repeat=1;
+                            re.x1=i;re.y1=j;re.z1=k;
+                            re.x2=hash[idx][ii].x;
+                            re.y2=hash[idx][ii].y;
+                            re.z2=hash[idx][ii].z;
+                            hash[idx][ii].empty=1;
+                            break;
+                        }
+                    }
+                    if(!repeat){
+                        hash[idx][hashcount[idx]].x=i;
+                        hash[idx][hashcount[idx]].y=j;
+                        hash[idx][hashcount[idx]].z=k;
+                        hash[idx][hashcount[idx]].empty=0;
+                        hashcount[idx]++;
+                    }         
+                }
+            }          
+        }
+    }
+    repeat=1;
+    int idx1,idx2;
+    while(repeat){
+        repeat=0;       
+        printf("%s\n",cubic[re.x1][re.y1][re.z1]);         
+        if(re.x1<N-1){
+            re.x1++;
+            idx1=f(cubic[re.x1][re.y1][re.z1],K);           
+            for(int ii=0;ii<C;ii++){               
+                int x=hash[idx1][ii].x,y=hash[idx1][ii].y,z=hash[idx1][ii].z;
+                if(!strcmp(cubic[x][y][z],cubic[re.x1][re.y1][re.z1])&&!hash[idx1][ii].empty){                
+                    repeat=1;                
+                    if(re.x2<N-1){
+                        re.x2++;
+                        idx2=f(cubic[re.x2][re.y2][re.z2],K);
+                        for(int ii=0;ii<C;ii++){
+                            if(hash[idx2][ii].empty){
+                                hash[idx2][ii].x=re.x2;
+                                hash[idx2][ii].y=re.y2;
+                                hash[idx2][ii].z=re.z2;
+                                hash[idx2][ii].empty=0;
+                                break;
+                            }
+                        }
+                    }                                  
+                    re.x2=hash[idx1][ii].x;
+                    re.y2=hash[idx1][ii].y;
+                    re.z2=hash[idx1][ii].z;
+                    hash[idx1][ii].empty=1;
+                    break;
+                }
+            }
+            if(!repeat){ // re.x1 doesn't find repeat
+                for(int ii=0;ii<C;ii++){
+                    if(hash[idx1][ii].empty){
+                        hash[idx1][ii].x=re.x1;
+                        hash[idx1][ii].y=re.y1;
+                        hash[idx1][ii].z=re.z1;
+                        hash[idx1][ii].empty=0;
+                        
+                        break;
+                    }
+                }
+                if(re.x2<N-1){                  
+                    re.x2++;
+                    idx2=f(cubic[re.x2][re.y2][re.z2],K);
+                    for(int ii=0;ii<C;ii++){
+                        int x=hash[idx2][ii].x,y=hash[idx2][ii].y,z=hash[idx2][ii].z;
+                        if(!strcmp(cubic[x][y][z],cubic[re.x2][re.y2][re.z2])&&!hash[idx2][ii].empty){                
+                            repeat=1;                   
+                            re.x1=hash[idx2][ii].x;
+                            re.y1=hash[idx2][ii].y;
+                            re.z1=hash[idx2][ii].z;
+                            hash[idx2][ii].empty=1;
+                            break;
+                        }
+                    }           
+                }              
+            }
+        }
+        else{
+            if(re.x2<N-1){
+                re.x2++;
+                idx2=f(cubic[re.x2][re.y2][re.z2],K);
+                for(int ii=0;ii<C;ii++){
+                    int x=hash[idx2][ii].x,y=hash[idx2][ii].y,z=hash[idx2][ii].z;
+                    if(!strcmp(cubic[x][y][z],cubic[re.x2][re.y2][re.z2])&&!hash[idx2][ii].empty){                
+                        repeat=1;                   
+                        re.x1=hash[idx2][ii].x;
+                        re.y1=hash[idx2][ii].y;
+                        re.z1=hash[idx2][ii].z;
+                        hash[idx2][ii].empty=1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
---
+++ C2022/50240/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct{
+    char s[101];
+    int x;
+    int y;
+}heapdata;
+int LEFT(int i) {
+    return 2 * i;
+}
+int RIGHT(int i) {
+    return 2 * i + 1;
+}
+int PARENT(int i) {
+    return i / 2;
+}
+int GET_LARGEST(heapdata A[], int i, int heapsize) {
+    int l = LEFT(i), r = RIGHT(i), largest;
+    if (l <= heapsize && strcmp(A[l].s,A[i].s)>0)largest = l;
+    else largest = i;
+    if (r <= heapsize && strcmp(A[r].s,A[largest].s)>0)largest = r;
+    return largest;
+}
+void MAX_HEAPIFY(heapdata A[], int i, int heapsize) {
+    int largest = GET_LARGEST(A, i, heapsize);
+    heapdata temp;
+    while (largest != i) {
+        temp=A[i];
+        A[i]=A[largest];
+        A[largest]=temp;
+        i = largest; largest = GET_LARGEST(A, i, heapsize);
+    }
+}
+void BUILD_MAX_HEAP(heapdata A[], int heapsize) {
+    for (int i = heapsize / 2; i > 0; i--) {
+        MAX_HEAPIFY(A, i, heapsize);
+    }
+}
+void HEAP_EXTRACT_MAP(heapdata A[], int heapsize) {
+    assert(heapsize >= 1);
+    printf("%s\n", A[1].s); 
+    A[1]=A[heapsize];
+    heapsize--;
+    MAX_HEAPIFY(A, 1, heapsize);
+}
+void MAX_HEAP_INSERT(heapdata A[], char* key, int heapsize,int x,int y) {
+    heapsize++; int i = heapsize; heapdata temp;
+    strcpy(A[i].s,key);
+    A[i].x=x;A[i].y=y;
+    while (i > 1 && strcmp(A[PARENT(i)].s,A[i].s)<0) {
+        temp=A[i];
+        A[i]=A[PARENT(i)];
+        A[PARENT(i)]=temp;
+        i = PARENT(i);
+    }
+}
+int main() {
+    heapdata *heaparray; int heapsize=0, operanum, operation;
+    char***tower,str[101];
+    int n,m,**re,x,y;
+    scanf("%d%d",&n,&m);
+    heaparray=(heapdata*)malloc((n*n+1)*sizeof(heapdata));
+    tower=(char***)malloc(m*sizeof(char**));
+    re=(int**)malloc(n*sizeof(int*));
+    for(int i=0;i<n;i++){
+        re[i]=(int*)malloc(n*sizeof(int));        
+    }
+    for(int i=0;i<m;i++){
+        tower[i]=(char**)malloc(n*sizeof(char*));
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            tower[i][j]=(char*)malloc(n*sizeof(char));
+        }
+    }
+    for(int i=0;i<n;i++)memset(re[i],0,n*sizeof(int));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%s",tower[i][j]);
+        }
+    } 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(tower[0][i][j]=='0'){
+                re[i][j]++;
+            }
+            else{
+                for(int k=0;tower[k][i][j]!='0';k++){
+                    str[k]=tower[k][i][j];
+                    re[i][j]++;
+                }
+                str[re[i][j]++]='\0';
+                MAX_HEAP_INSERT(heaparray,str,heapsize,i,j);
+                heapsize++;
+            }
+        }
+    }  
+    BUILD_MAX_HEAP(heaparray, heapsize);
+    while(heapsize>0){
+        x=heaparray[1].x;y=heaparray[1].y;
+        HEAP_EXTRACT_MAP(heaparray, heapsize);heapsize--;
+        if(re[x][y]<m){
+            int start=re[x][y];
+            for(int i=start;tower[i][x][y]!='0';i++){
+                str[i-start]=tower[i][x][y];
+                re[x][y]++;
+            }           
+            str[re[x][y]-start]='\0';
+            re[x][y]++;
+            MAX_HEAP_INSERT(heaparray,str,heapsize,x,y);
+            heapsize++;
+        }
+    }
+    free(heaparray);
+    for(int i=0;i<n;i++)free(re[i]);
+    free(re);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            free(tower[i][j]);
+        }
+    }
+    for(int i=0;i<m;i++)free(tower[i]);
+    free(tower);
+    return 0;
+}
