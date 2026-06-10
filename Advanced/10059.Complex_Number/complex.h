f149f0a44b50f1f6a0345d1ce1ee3ad04654efbe
YunMunJR < GitHub Gmail>
2026-06-10 20:32:05 +0800

    add header

--- Advanced/10059.Complex_Number/complex.h
+++ Advanced/10059.Complex_Number/complex.h
@@ -1,9 +1,10 @@
 #ifndef _COMPLEX_H
 #define _COMPLEX_H
  
-/*
-    define structure complex
-*/
+typedef struct complex{
+    float real ;
+    float unreal ;
+} complex ;
  
 void init(complex *c, float a, float b);
 void complex_print(complex *x);
